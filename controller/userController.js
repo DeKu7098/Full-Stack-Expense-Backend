00b807727fa426341655c40484d029ff8abbd13f
Arka Chakraborty <arkac720@gmail.com>
2023-07-09 00:06:13 +0530

    bcrypt added

--- controller/userController.js
+++ controller/userController.js
@@ -1,19 +1,28 @@
 const Users = require("../model/user.js");
+const bcrypt = require("bcrypt");
 
 exports.addUser = async (req, res, next) => {
   try {
-    const { name, email, password } = req.body;
+    let { name, email, password } = req.body;
     const search = await Users.findOne({ where: { email: email } });
     if (search) {
       console.log("email already exist");
       res.status(403).json({ message: "email exist" });
     } else {
-      await Users.create({
-        name: name,
-        email: email,
-        password: password,
+      const saltRounds = 10;
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        password = hash;
+        if (err) {
+          res.status(500).json({ message: "Something went wrong" });
+        } else {
+          await Users.create({
+            name: name,
+            email: email,
+            password: password,
+          });
+          res.status(201).json({ message: "updated" });
+        }
       });
-      res.status(201).json({ message: "updated" });
     }
   } catch (err) {
     res.status(500).json({ message: "Something went wrong" });
@@ -26,13 +35,15 @@ exports.logIn = async (req, res, next) => {
     const user = await Users.findOne({ where: { email: email } });
     // console.log(user,'hi');
     if (user) {
-      if( user.password === password ){
-        res.status(200).json({
-          message: "Logged in Successfully",
-        });
-      } else {
-        res.status(401).json({message: "Wrong Password"});
-      }
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (result) {
+          res.status(200).json({
+            message: "Logged in Successfully",
+          });
+        } else {
+          res.status(401).json({ message: "Wrong Password" });
+        }
+      })
     } else {
       res.status(404).json({
         message: "User not found",
