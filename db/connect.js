02a52b4cc466dc24317e177b8da5727704c35a3a
The_noob_dev <91478335+DeKu7098@users.noreply.github.com>
2023-07-18 23:20:07 +0530

    Add files via upload

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 const mysql = require("mysql2");
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_ADMIN, process.env.DB_PASS, {
-  host: "localhost",
+  host: process.env.DB_HOST,
   dialect: "mysql",
 });
 
